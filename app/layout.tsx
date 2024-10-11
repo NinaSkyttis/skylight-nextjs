947f0b959dbfc5a40e0ece9bd026afa08914854b
Nina Skyttmo <59294246+NinaSkyttis@users.noreply.github.com>
2024-10-10 20:08:48 -0400

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Banner } from "./components/banner";
@@ -29,6 +30,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
